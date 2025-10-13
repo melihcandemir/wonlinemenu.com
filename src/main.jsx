0a013230de633b9e592c27027c6009fb86c1a266
Melih Can Demir <melihcanndemir@gmail.com>
2025-10-13 15:09:26 +0300

    Title güncellendi, HasRooter kaldırıldı ve BrowserRouter eklendi, gelişim modunda localde sorunsuz çalışabilir ancak Github Page için güncelleme yapılmalı (bir önceki commit 00:18 de yapıldı) 15:09 13.10.2025

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
